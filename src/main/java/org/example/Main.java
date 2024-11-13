84d1c666a71bb43d3d106b952a84a0c6a72c12b1
Narzulaev Evgeni <narzulaev2@gmail.com>
2024-11-13 14:30:31 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.io.File;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        File file = new File("E:\\обои gr");
+        System.out.println(getFolderFile(file));
+    }
+    public static long getFolderFile(File folder){
+        if (folder.isFile()){
+            return folder.length();
+        }
+        long sum = Arrays.stream(folder.listFiles()).mapToLong(Main::getFolderFile).sum();
+        return sum;
     }
 }
